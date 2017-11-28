511cf520fb62a998c216d95bd5793f395735aed5
Oscar Blomqvist <oscar.blomqvist@gmail.com>
2017-11-28 12:51:36 +0100

    Experemented with corpses.

--- Assets/Scripts/EnemyHealthManager.cs
+++ Assets/Scripts/EnemyHealthManager.cs
@@ -7,6 +7,7 @@ public class EnemyHealthManager : MonoBehaviour {
 
 	public int health;
 	private int currentHealth;
+    public GameObject corpse;
 
 	// Use this for initialization
 	void Start () {
@@ -16,9 +17,14 @@ public class EnemyHealthManager : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (currentHealth <= 0) {
-			Destroy (gameObject);
+            if(corpse != null) {
+                corpse.GetComponent<Renderer>().enabled = true;
+                corpse.transform.parent = null;
+            }
+            Destroy (gameObject);
 			ScoreManager.addPoint ();
-		}
+
+        }
 	}
 
 	public void HurtEnemy(int damage){
