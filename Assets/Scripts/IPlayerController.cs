bf7a01d3020698503ef8bcfb8df8d6174279ad2c
Oscar Blomqvist <oscar.blomqvist@gmail.com>
2017-12-01 15:16:49 +0100

    Added floatiness when moving.

--- Assets/Scripts/IPlayerController.cs
+++ Assets/Scripts/IPlayerController.cs
@@ -5,5 +5,13 @@ using UnityEngine;
 public abstract class IPlayerController : MonoBehaviour {
 
     public IGun theGun;
+    public float floatiness = 8;
+    protected Vector3 lastMovementVector = Vector3.zero;
+
+    protected Vector3 GetMovementVector (Vector3 wantedMovementVector, float deltaTime) {
+        Vector3 movementVector = Vector3.Lerp(lastMovementVector, wantedMovementVector, floatiness * deltaTime);
+        lastMovementVector = movementVector;
+        return movementVector;
+    }
 
 }
