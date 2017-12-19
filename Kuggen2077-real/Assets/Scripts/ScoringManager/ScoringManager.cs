db9f1969046f4c69f1c6427e4e87b7b0e07416ff
Felixfranzen <felixfranzn@gmail.com>
2017-12-19 12:08:35 +0100

    basic score handling

---
+++ Kuggen2077-real/Assets/Scripts/ScoringManager/ScoringManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoringManager {
+	
+	private Dictionary<string, int> scores = new Dictionary<string, int>();
+
+	private static ScoringManager instance;
+	private ScoringManager() {}
+
+	public static ScoringManager Instance {
+		get {
+			if (instance == null) {
+				instance = new ScoringManager();
+			}
+			return instance;
+		}
+	}
+
+	public void incrementScore(string playerId){
+		scores [playerId] = scores [playerId] + 1;
+	}
+
+	public int getScore(string playerId){
+		return scores [playerId];
+	}
+
+	public Dictionary<string, int> getAllScores(){		
+		return scores;
+	}
+}
---
+++ Kuggen2077-real/Assets/Scripts/ScoringManager/ScoringViewManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoringViewManager : MonoBehaviour {
+
+	private ScoringManager manager;
+	private ScoreView[] scoreViews;
+
+	// Use this for initialization
+	void Start () {
+		manager = ScoringManager.Instance;
+		scoreViews = GetComponentsInChildren<ScoreView>();
+		Debug.Log (scoreViews);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Dictionary<string, int> scores = manager.getAllScores ();
+		foreach (ScoreView view in scoreViews) {
+			if (scores.ContainsKey(view.id)) {
+				view.setScore (scores [view.id]);
+			}
+		}
+	}
+
+}
