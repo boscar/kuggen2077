3e6bd13a9cd88e483ccf28a5bd03a003c904a687
Felixfranzen <felixfranzn@gmail.com>
2018-01-03 16:10:14 +0100

    working ui

--- Kuggen2077-real/Assets/Scripts/PlayerUI/PlayerUIController.cs
+++ Kuggen2077-real/Assets/Scripts/PlayerUI/PlayerUIController.cs
@@ -1,21 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerUIController : MonoBehaviour, IObserver<Player> {
 
 	public Player playerReference;
 
-	void Start () {
+	private Slider healthSlider;
+	private Text scoreText;
+	private Text weaponText;
+
+	void Awake () {
 		playerReference.addObserver (this);
+		InitUI ();
+	}
+
+	private void InitUI () {
+		healthSlider = GameObject.FindGameObjectWithTag ("Health").GetComponent<Slider>();
+		scoreText = GameObject.FindGameObjectWithTag ("Score").GetComponent<Text>();
+		weaponText = GameObject.FindGameObjectWithTag ("Weapon").GetComponent<Text>();
+
+		if (healthSlider == null) {
+			throw new KuggenException("UI Slider object tagged 'Health' missing from " + this);
+		}	
+
+		if (scoreText == null) {
+			throw new KuggenException("UI Text object tagged 'Score' missing from " + this);
+		}	
+
+		if (weaponText == null) {
+			throw new KuggenException("UI Text object tagged 'Weapon' missing from " + this);
+		}	
 	}
 	
 	public void onUpdate(Player player) {
-		Debug.Log ("---------------");
-		Debug.Log (Utils.LeftPad(player.Score.ToString(), 2, "0"));
-		Debug.Log (player.CurrentHitPoints);
-		AttackAction atk = player.AttackActions [Player.ATTACK_PRIMARY];
-		Debug.Log (atk.displayName);
-		Debug.Log ("---------------");
+		healthSlider.value = (float) player.CurrentHitPoints / (float)player.HitPoints;
+		scoreText.text = Utils.LeftPad (player.Score.ToString (), 2, "0");
+		weaponText.text = player.AttackActions [Player.ATTACK_PRIMARY].displayName;
 	}
 }
