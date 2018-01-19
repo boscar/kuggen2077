5bad16b63e7fec3e9b2ba6777c079635c6d08ce2
Oscar Blomqvist <oscar.blomqvist@gmail.com>
2018-01-19 13:35:26 +0100

    Made GetKnockbackVector be part of an attack.

--- Kuggen2077-real/Assets/Scripts/Combat/Attack.cs
+++ Kuggen2077-real/Assets/Scripts/Combat/Attack.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 
 
@@ -11,11 +12,21 @@ public class Attack {
     public float Force { get; private set; }
     public Vector3 Position { get; private set; }
 
-    public Attack(IAttacker attacker, int damage, float force, Vector3 position) {
+    public Func<Attack, IAttackable, Vector3> GetKnockbackVector { get; private set; }
+
+    public Attack(IAttacker attacker, int damage, float force, Vector3 position) : this(attacker, damage, force, position, 
+        (Attack attack, IAttackable attackable) => {
+            Vector3 attackPos = new Vector3(attack.Position.x, 0, attack.Position.z);
+            Vector3 targetPos = new Vector3(attackable.Transform.position.x, 0, attackable.Transform.position.z);
+            return (targetPos - attackPos).normalized * attack.Force;
+        }) { }
+
+    public Attack(IAttacker attacker, int damage, float force, Vector3 position, Func<Attack, IAttackable, Vector3> getKnockbackVector) {
         Attacker = attacker;
         Damage = damage;
         Force = force;
         Position = position;
+        GetKnockbackVector = getKnockbackVector;
     }
 
 }
