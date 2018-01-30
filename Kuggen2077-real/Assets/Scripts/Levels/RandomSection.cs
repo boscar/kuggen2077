08651b88f8a756e2fffb00f13fc5d0bbac043261
Oscar Blomqvist <oscar.blomqvist@gmail.com>
2018-01-30 17:04:22 +0100

    Refactored random section a bit.

--- Kuggen2077-real/Assets/Scripts/Levels/RandomSection.cs
+++ Kuggen2077-real/Assets/Scripts/Levels/RandomSection.cs
@@ -7,8 +7,10 @@ public class RandomSection : Section {
     public const float SHORT = 30;
     public const float MEDIUM = 45;
     public const float LONG = 65;
-
+    
     private const float SPAWN_INTERVAL = 0.4f;
+    private const float SEGMENT_DURATION = 15f;
+    private const float SEGMENT_DURATION_VARIATION = 3f;
 
     public RandomSection (int index, float duration, float strength, Transform[] spawnPoints, Enemy[] enemyObjects) : base(index, DEFAULT_POWERUP_SPAWN_CHANCE, DEFAULT_POWERUPS) {
         Events = GenerateEnemyEvents(duration, strength, spawnPoints, enemyObjects);
@@ -16,11 +18,8 @@ public class RandomSection : Section {
 
     private List<Event> GenerateEnemyEvents(float duration, float strength, Transform[] spawnPoints, Enemy[] enemyObjects) {
         List<Event> events = new List<Event>();
-
-        float spawnInterval = GetSpawnInterval(strength);
-        float intervalOffset = spawnInterval * 0.15f;
         
-        List<float> spawnTimestamps = GetSpawnTimestamps(duration, spawnInterval, intervalOffset);
+        List<float> spawnTimestamps = GetSpawnTimestamps(duration);
 
         for (int i = 0; i < spawnTimestamps.Count; ++i) {
                 events.AddRange(GetTimestampEvents(spawnTimestamps[i], strength, spawnPoints, enemyObjects, i == (spawnTimestamps.Count - 1)));
@@ -29,26 +28,22 @@ public class RandomSection : Section {
         return events;
     }
 
-    private List<float> GetSpawnTimestamps(float duration, float strength, float intervalOffset) {
+    private List<float> GetSpawnTimestamps(float duration) {
         List<float> timestamps = new List<float>();
         float timestamp = 0;
-        while(duration > (strength + intervalOffset)) {
-            timestamp = timestamp + strength - intervalOffset + (Random.value * (2 * intervalOffset));
+        while(duration > (SEGMENT_DURATION * 0.5f)) {
+            timestamp = timestamp + SEGMENT_DURATION - SEGMENT_DURATION_VARIATION + (Random.value * (2 * SEGMENT_DURATION_VARIATION));
             duration = duration - timestamp;
             timestamps.Add(timestamp);
         }
         return timestamps;
     }
 
-    private float GetSpawnInterval(float strength) {
-        return 6f + strength * 0.4f;
-    }
-
     private List<Event> GetTimestampEvents(float timestamp, float strength, Transform[] spawnPoints, Enemy[] enemyObjects, bool lastTimestamp) {
         List<Event> events = new List<Event>();
 
         int strengthOfEnemies = GetStrenthOfEnemies(strength);
-        List<Vector3> timestampSpawnPositions = GetTimestampSpawnPositions(spawnPoints, strengthOfEnemies);
+        List<Vector3> timestampSpawnPositions = GetTimestampSpawnPositions(spawnPoints);
         Enemy[] timestampEnemyObjects = GetTimestampEnemyObjects(enemyObjects);
 
         while(strengthOfEnemies >= 1) {
@@ -76,19 +71,20 @@ public class RandomSection : Section {
         return (int)(baseAmount - offset + (Random.value * (2 * offset)));
     }
 
-    private List<Vector3> GetTimestampSpawnPositions(Transform[] spawnPoints, int strength) {
+    private List<Vector3> GetTimestampSpawnPositions(Transform[] spawnPoints) {
         List<Vector3> spawnPositions = new List<Vector3>();
-            spawnPositions.Add(Utils.GetRandom<Transform>(spawnPoints).position);
-        if (strength >= 9) {
-            spawnPositions.Add(Utils.GetRandom<Transform>(spawnPoints).position);
-        }
-        if (strength >= 7) {
+        spawnPositions.Add(Utils.GetRandom<Transform>(spawnPoints).position);
+        float randomValue = Random.value;
+        if (randomValue > 0.2f) {
             spawnPositions.Add(Utils.GetRandom<Transform>(spawnPoints).position);
         }
-        if (strength >= 5) {
+        if (randomValue > 0.4f) {
             spawnPositions.Add(Utils.GetRandom<Transform>(spawnPoints).position);
         }
-        if (strength >= 3) {
+        if (randomValue > 0.6f) {
+            spawnPositions.Add(Utils.GetRandom<Transform>(spawnPoints).position);
+        }
+        if (randomValue > 0.8f) {
             spawnPositions.Add(Utils.GetRandom<Transform>(spawnPoints).position);
         }
         return spawnPositions;
