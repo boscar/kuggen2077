890c87f12797f9c7e9166414827628cb20ff305d
Felixfranzen <felixfranzn@gmail.com>
2018-02-14 09:31:29 +0100

    show the correct players on play

---
+++ Kuggen2077-real/Assets/Scripts/PlayerSetupManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSetupManager : MonoBehaviour {
+
+	public GameObject playerOne;
+	public GameObject playerTwo;
+
+	// Use this for initialization
+	void Start () {
+		switch (HighScoreManager.Players) {
+		case HighScoreManager.PlayerState.PLAYER_ALL:
+			playerOne.SetActive (true);
+			playerTwo.SetActive (true);
+			break;
+
+		case HighScoreManager.PlayerState.PLAYER_ONE:
+			playerOne.SetActive (true);
+			break;
+
+		case HighScoreManager.PlayerState.PLAYER_TWO:
+			playerTwo.SetActive (true);
+			break;
+
+		default:
+			break;
+		}
+	}
+}
